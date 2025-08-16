f3cdfa5b7999236cd24b51d27310970a4a30b7ef
Andryss <andr.krivosheev@gmail.com>
2025-08-16 12:14:33 +0300

    Initial commit

---
+++ src/main/java/ru/andryss/antalk/server/AnTalkBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.andryss.antalk.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnTalkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnTalkBackendApplication.class, args);
+    }
+
+}
