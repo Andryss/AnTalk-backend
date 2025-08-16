ecfc8016c533c8525286ae530dc4b0c971feb8c5
Andryss <andr.krivosheev@gmail.com>
2025-08-16 12:26:11 +0300

    Add common checks

---
+++ README.md
@@ -0,0 +1,3 @@
+## AnTalk backend server
+
+TBD
--- src/main/java/ru/andryss/antalk/server/AnTalkBackendApplication.java
+++ src/main/java/ru/andryss/antalk/server/AnTalkBackendApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AnTalkBackendApplication {
 
+    /**
+     * Точка входа в приложение
+     */
     public static void main(String[] args) {
         SpringApplication.run(AnTalkBackendApplication.class, args);
     }
